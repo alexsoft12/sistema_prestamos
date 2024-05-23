4015e50cd82ea1d33a6248039523b1ae2cc1a1c5
Yuler Figueroa <yuleralex1@gmail.com>
2024-05-22 20:07:22 -0500

    ms-clientes init

---
+++ ms-clientes/src/test/java/pe/a3ya/msclientes/MsClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.a3ya.msclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
