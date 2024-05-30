8ddfbce38eec77853fce4a3cdc47084dc6ac3345
Frank97AV <fraya28427@gmail.com>
2024-05-29 21:25:29 -0500

    ms-customers: Request validation

--- ms-customers/domain/src/main/java/pe/a3ya/mscustomers/domain/aggregates/request/AddressRequest.java
+++ ms-customers/domain/src/main/java/pe/a3ya/mscustomers/domain/aggregates/request/AddressRequest.java
@@ -1,5 +1,7 @@
 package pe.a3ya.mscustomers.domain.aggregates.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,14 +9,34 @@ import lombok.Setter;
 @Setter
 public class AddressRequest {
     private Long id;
+    @NotNull(message = "department is mandatory")
+    @NotBlank(message = "department is mandatory")
     private String department;
+    @NotNull(message = "province is mandatory")
+    @NotBlank(message = "province is mandatory")
     private String province;
+    @NotNull(message = "district is mandatory")
+    @NotBlank(message = "district is mandatory")
     private String district;
+    @NotNull(message = "address is mandatory")
+    @NotBlank(message = "address Number is mandatory")
     private String address;
+    @NotNull(message = "street is mandatory")
+    @NotBlank(message = "street is mandatory")
     private String street;
+    @NotNull(message = "number is mandatory")
+    @NotBlank(message = "number is mandatory")
     private String number;
+    @NotNull(message = "reference is mandatory")
+    @NotBlank(message = "reference is mandatory")
     private String reference;
+    @NotNull(message = "postalCode is mandatory")
+    @NotBlank(message = "postalCode is mandatory")
     private String postalCode;
+    @NotNull(message = "latitude is mandatory")
+    @NotBlank(message = "latitude is mandatory")
     private String latitude;
+    @NotNull(message = "longitude is mandatory")
+    @NotBlank(message = "longitude is mandatory")
     private String longitude;
 }
--- ms-customers/domain/src/main/java/pe/a3ya/mscustomers/domain/aggregates/request/CustomerRequest.java
+++ ms-customers/domain/src/main/java/pe/a3ya/mscustomers/domain/aggregates/request/CustomerRequest.java
@@ -1,5 +1,7 @@
 package pe.a3ya.mscustomers.domain.aggregates.request;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,9 +11,21 @@ import java.util.List;
 @Getter
 @Setter
 public class CustomerRequest {
+    @Valid
+    @NotNull(message = "Document Number is mandatory")
+    @NotBlank(message = "Document Number is mandatory")
     private String documentNumber;
+    @NotNull(message = "Email is mandatory")
+    @NotBlank(message = "Email is mandatory")
     private String email;
+    @NotNull(message = "Phone is mandatory")
+    @NotBlank(message = "Phone is mandatory")
     private String phone;
+    @NotNull(message = "BirthDate is mandatory")
+    @PastOrPresent(message = "BirthDate is mandatory")
     private LocalDate birthDate;
+    @NotNull(message = "Address is mandatory")
+    @NotEmpty(message = "Address is mandatory")
+    @Valid
     private List<AddressRequest> addresses;
 }
