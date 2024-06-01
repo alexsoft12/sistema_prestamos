aa6c43556b25a10ab458d34338a4190fcec9cf51
bryanhuaman <bryanhuamanchavez29@gmail.com>
2024-06-01 04:37:47 -0500

    ms-*: optimizated valide security

---
+++ ms-customers/src/main/java/pe/a3ya/mscustomers/infrastructure/adapters/SecurityValidator.java
@@ -0,0 +1,45 @@
+package pe.a3ya.mspayments.infrastructure.adapters;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import pe.a3ya.mspayments.domain.aggregates.request.TokenRequest;
+import pe.a3ya.mspayments.infrastructure.clients.SecurityClient;
+
+@Service
+public class SecurityValidator {
+
+    private final SecurityClient securityClient;
+
+    public SecurityValidator(SecurityClient securityClient) {
+        this.securityClient = securityClient;
+    }
+
+    public void validateSecurity() {
+        final String authHeader = getAuthHeader();
+        final String jwt;
+        if (!StringUtils.hasLength(authHeader) || !StringUtils.startsWithIgnoreCase(authHeader, "Bearer ")) {
+            throw new IllegalArgumentException("Token no valido");
+        }
+        jwt = authHeader.substring(7);
+
+        TokenRequest bodyToken = new TokenRequest();
+        bodyToken.setToken(jwt);
+
+        boolean res = securityClient.getSecurityToken(bodyToken);
+        if (!res) {
+            throw new IllegalArgumentException("Token no valido");
+        }
+    }
+
+    private static String getAuthHeader() {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes == null) {
+            throw new IllegalStateException("No request attributes found. This method must be called in the context of an HTTP request.");
+        }
+        HttpServletRequest request = attributes.getRequest();
+        return request.getHeader("Authorization");
+    }
+}
